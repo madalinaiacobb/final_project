2df6aa39c2283b3de0af5026fef59a5175a81f15
Madalina Iacob <madalina.iacob1995@gmail.com>
2023-03-15 19:24:58 +0200

    searchbycategory

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -4,14 +4,10 @@ function ProductComponent({ product }) {
 	return (
 		<Col className='mt-4'>
 			<Card>
-				<img
-					alt='Sample'
-					src={"https://picsum.photos/300/200?random=" + product.id}
-				/>
+				<img alt='Sample' src={product.image} weight='200' height='300' />
 				<CardBody>
 					<CardTitle tag='h4'>{product.title}</CardTitle>
 					<CardText>{product.description}</CardText>
-					<h6>{product.id}</h6>
 					<Button>Add to wishlist</Button>
 				</CardBody>
 			</Card>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
-import { Spinner, Input, Row } from "reactstrap";
+import { Spinner, Input, Row, Button, Badge } from "reactstrap";
 import ProductComponent from "../components/ProductComponent";
 import "./Products.style.css";
 
 function Products() {
 	const [productList, setProductList] = useState(null);
-	const [categorieList, setCategorieList] = useState(null);
+	const [categoryList, setCategoryList] = useState(null);
+
+	// Variabile de stare pt filtrari
+
+	const [textInput, setTextInput] = useState("");
+	const [selectedCategory, setSelectedCategory] = useState(null);
 
 	// Pasul de adaugare date din API
 
@@ -16,31 +21,73 @@ function Products() {
 
 		fetch("https://fakestoreapi.com/products/categories")
 			.then((res) => res.json())
-			.then((categorieListAPI) => setCategorieList(categorieListAPI));
+			.then((categoryListAPI) => setCategoryList(categoryListAPI));
 	}, []);
 
 	return (
 		<>
-			{productList && categorieList ? (
+			{productList && categoryList ? (
 				<div className='d-flex'>
-					<div className='product_categories'>
-						<h3>Product Categories</h3>
-						{categorieList.map((categorie, index) => {
-							return <h5 key={"categorie_" + index}> - {categorie}</h5>;
+					<div className='product_categories m-3 d-flex flex-column'>
+						<h5>Product Categories</h5>
+						{categoryList.map((category, index) => {
+							return (
+								<Badge
+									className='mt-2'
+									key={"category_" + index}
+									color={category === selectedCategory ? "success" : "dark"}
+									onClick={() => {
+										setSelectedCategory(category);
+									}}>
+									{category}
+								</Badge>
+							);
 						})}
+						<Button
+							className='mt-3'
+							color='dark'
+							size='sm'
+							onClick={() => {
+								setSelectedCategory(null);
+							}}>
+							Clear Filter!
+						</Button>
 					</div>
 					<div className='product_list'>
 						<h5>Search</h5>
-						<Input className='w-25' />
-						<Row>
-							{productList.map((product, index) => {
-								return (
-									<ProductComponent
-										product={product}
-										key={"product_" + index}
-									/>
-								);
-							})}
+						<Input
+							className='w-25'
+							value={textInput}
+							onChange={(event) => {
+								setTextInput(event.target.value);
+							}}
+						/>
+						<Row xs='4'>
+							{productList
+								.filter((product) => {
+									return (
+										selectedCategory === null ||
+										selectedCategory === product.category
+									);
+								})
+								.filter((product) => {
+									return (
+										product.description
+											.toLowerCase()
+											.includes(textInput.toLowerCase()) ||
+										product.title
+											.toLowerCase()
+											.includes(textInput.toLowerCase())
+									);
+								})
+								.map((product, index) => {
+									return (
+										<ProductComponent
+											product={product}
+											key={"product_" + index}
+										/>
+									);
+								})}
 						</Row>
 					</div>
 				</div>
