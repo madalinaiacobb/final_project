9dcddd030bc7ca3844a483b2bc5aac4f4db890c2
Madalina Iacob <madalina.iacob1995@gmail.com>
2023-03-29 10:54:50 +0300

    homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect } from "react";
-import { Spinner, Row, UncontrolledCarousel } from "reactstrap";
+import {
+	Spinner,
+	Row,
+	UncontrolledCarousel,
+	Card,
+	CardTitle,
+	Button,
+	CardBody,
+} from "reactstrap";
 import TrendingComponent from "../components/TrendingComponent";
 import "./Home.style.css";
 
 function Home() {
 	const [trendingList, setTrendingList] = useState(null);
+	const [categoriesList, setCategoriesList] = useState(null);
 
 	useEffect(() => {
 		fetch("https://fakestoreapi.com/products?limit=8")
 			.then((res) => res.json())
 			.then((trendingListAPI) => setTrendingList(trendingListAPI));
+
+		fetch("https://fakestoreapi.com/products/categories")
+			.then((res) => res.json())
+			.then((categoriesListAPI) => setCategoriesList(categoriesListAPI));
 	}, []);
 
 	return (
@@ -38,10 +51,32 @@ function Home() {
 					},
 				]}
 			/>
+			<div className='categories_list m-3 d-flex flex-row'>
+				{categoriesList.map((categories, index) => {
+					return (
+						<Row className='mt-2' key={"categories_" + index}>
+							<Card className='text-center' style={{ minHeight: "100%" }}>
+								<img
+									alt='Sample'
+									src='https://picsum.photos/id/26/300/200'
+									weight='50'
+									height='150'
+								/>
+								<CardBody>
+									<CardTitle tag='h5' className='mt-auto'>
+										{categories}
+									</CardTitle>
 
+									<Button className='mt-2' color='dark' size='sm'>
+										Discover Now
+									</Button>
+								</CardBody>
+							</Card>
+						</Row>
+					);
+				})}
+			</div>
 			<div className='home_page d-flex'>
-				<div className='categories_list'></div>
-
 				<div className='trending_list'>
 					<div className='titletrending_products'>
 						<h3>Trending Products</h3>
